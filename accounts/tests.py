f661097f29555a883d616594c249bf258353b28c
fadiHB <eng.fadi.hb@gmail.com>
2021-01-09 21:33:23 +0200

    test

--- accounts/tests.py
+++ accounts/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
-class CreatUsersTests(TestCase):
+class CreateUsersTests(TestCase):
 
     def test_create_user(self):
         self.user = get_user_model().objects.create_user(
@@ -13,22 +13,3 @@ class CreatUsersTests(TestCase):
             last_name = 'hb'
         )
         self.assertEqual(self.user.email, 'fadi@gmail.com')
-
-    def test_create_duplicate_user(self):        
-        self.user = get_user_model().objects.create_user(
-            username = 'fadi', 
-            email='fadi@gmail.com', 
-            password='fadi123456',
-            first_name = 'fadi',
-            last_name = 'hb'
-        )
-
-        
-    def test_create_none_user(self):
-        self.user = get_user_model().objects.create_user(
-            username = '', 
-            email='', 
-            password='',
-            first_name = '',
-            last_name = ''
-        )
